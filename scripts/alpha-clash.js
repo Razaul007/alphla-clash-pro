7e62be9b67d7157cf41625720ab18ef010465f4a
Razaul007 <raselharuyamare@gmail.com>
2024-03-03 04:45:25 +0600

    added first javascripts for play-ground

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,8 @@
+function play(){
+    const homeSection = document.getElementById('home-screen');
+    homeSection.classList.add('hidden');
+
+    const playGroundSection = document.getElementById('play-ground');
+    playGroundSection.classList.remove('hidden');
+    // console.log(playGroundSection.classList);
+}
