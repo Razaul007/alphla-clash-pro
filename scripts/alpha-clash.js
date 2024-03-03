9ab018211292f20513e37c8381c76acbb5ce2340
Razaul007 <raselharuyamare@gmail.com>
2024-03-04 01:03:26 +0600

    added keyboard functionality

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -8,9 +8,33 @@ function play(){
     continueGame();
 }
 
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    console.log('Player Pressed', playerPressed)
+    // console.log(event.key);
+
+    // expected press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+    console.log(expectedAlphabet, playerPressed);
+
+    // checked matched or, not
+    if(playerPressed === expectedAlphabet){
+        console.log('You got a point.');
+        removeBackgroundColor(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('You Missed, You lost a life!')
+    }
+}
+// capture keyboard press
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
 function continueGame(){
     const alphabet = getARandomAlphabet();
-    console.log('Your random alphabet', alphabet);
+    // console.log('Your random alphabet', alphabet);
     // show current alphabet
     const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
@@ -22,6 +46,12 @@ function continueGame(){
 function addBackgroundColor(elementId){
    const element = document.getElementById(elementId);
    element.classList.add('bg-orange-400');
+}
+
+// Remove alphabet background color
+function removeBackgroundColor(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.remove('bg-orange-400');
 }
 
 function getARandomAlphabet(){
